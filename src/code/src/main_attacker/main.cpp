be222b5a68e000761e08020c7532749a82910520
alfonsín <156380060+alfonsodeanda@users.noreply.github.com>
2026-02-05 20:54:00 -0600

    confugured build settings
    ready to start developing

--- src/code/src/main_attacker/main.cpp
+++ src/code/src/main_attacker/main.cpp
@@ -1,18 +1,11 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
+// the setup function runs once when you press reset or power the board
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
 }
 
+// the loop function runs over and over again forever
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
