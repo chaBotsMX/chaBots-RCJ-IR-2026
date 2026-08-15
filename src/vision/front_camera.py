c317748cfc1e9539d6251954294cacafba9efa08
alfonsín <156380060+alfonsodeanda@users.noreply.github.com>
2026-08-15 16:40:24 -0600

    added cam distance estimate

--- src/vision/front_camera.py
+++ src/vision/front_camera.py
@@ -17,15 +17,8 @@ ROI = (0, CROP_FROM_TOP, 320, 240 - CROP_FROM_TOP)
 FOV_HORIZONTAL = 71.8
 DEGREES_IN_PIXEL = FOV_HORIZONTAL / 320
 
-# Calibration points: (top_y_pixel, distance_in_cm)
-# Keep this list sorted from LOWEST top_y to HIGHEST top_y
-# needs to calibrate
-CALIBRATION_POINTS = [
-    (14, 200),
-    (16, 150),
-    (20, 100),
-    (22, 50)
-]
+BOTTOM_CENTER_X = 160
+BOTTOM_CENTER_Y = 239
 
 # Init CSI Camera
 csi0 = csi.CSI()
@@ -53,35 +46,11 @@ clock = time.clock()
 uart = UART(3, 115200, timeout_char=0)
 
 angle = 254
+distance = 254
+confidence = 254
 
 
-def estimate_distance(blob_y):
-    if blob_y <= CALIBRATION_POINTS[0][0]:
-        return CALIBRATION_POINTS[0][1]
-    if blob_y >= CALIBRATION_POINTS[-1][0]:
-        return CALIBRATION_POINTS[-1][1]
-
-    for i in range(len(CALIBRATION_POINTS) - 1):
-        y0, d0 = CALIBRATION_POINTS[i]
-        y1, d1 = CALIBRATION_POINTS[i + 1]
-
-        if y0 <= blob_y <= y1:
-            # Linear Interpolation Formula
-            fraction = (blob_y - y0) / (y1 - y0)
-            interpolated_distance = d0 + fraction * (d1 - d0)
-            return interpolated_distance
-    return 254
-
-
-def map_value(x, in_min, in_max, out_min, out_max):
-    if in_max == in_min:
-        return out_min
-
-    mapped = out_min + (x - in_min) * (out_max - out_min) / (in_max - in_min)
-    return max(min(int(mapped), out_max), out_min)
-
-
-def process_goal(blobs, min_confidence):
+def process_goal(blobs):
     if not blobs:
         return None
 
@@ -92,30 +61,61 @@ def process_goal(blobs, min_confidence):
 
     angle = int((largest_blob.cx - 160) * (DEGREES_IN_PIXEL))
 
-    return (angle, largest_blob)
+    dx = largest_blob.cx - BOTTOM_CENTER_X
+    dy = largest_blob.cy - BOTTOM_CENTER_Y
+    distance = int((dx**2 + dy**2) ** 0.5)
+
+    aspect_ratio = largest_blob.w / largest_blob.h
+    confidence = int((aspect_ratio * 100) / 2.5)
+
+    return (angle, distance, confidence, largest_blob)
 
 
 while True:
     clock.tick()
     img = csi0.snapshot()
 
-    img.lens_corr(strength=1)
+    img.lens_corr(strength=1.8)
 
-    all_blobs = img.find_blobs(THRESHOLDS, pixels_threshold=100, merge=True, x_stride=4, y_stride=2)
-    yellow_blobs = [b for b in all_blobs if b.code == 1]  # 1st threshold in list
-    blue_blobs = [b for b in all_blobs if b.code == 2]    # 2nd threshold in list
-
-    yellow_goal = process_goal(yellow_blobs, 0)
-    blue_goal = process_goal(blue_blobs, 0)
-
-    current_goal = yellow_goal if TARGET == "YELLOW" else blue_goal
+    if TARGET == "YELLOW":
+        current_goal = process_goal(
+            img.find_blobs(
+                [YELLOW_THRESHOLD],
+                pixels_threshold=100,
+                merge=True,
+                x_stride=4,
+                y_stride=2,
+            )
+        )
+    else:
+        current_goal = process_goal(
+            img.find_blobs(
+                [BLUE_THRESHOLD],
+                pixels_threshold=100,
+                merge=True,
+                x_stride=4,
+                y_stride=2,
+            )
+        )
 
     if current_goal:
-        angle, goal_blob = current_goal
-        # draw_rectangle now expects a coordinate tuple; blob.rect() returns one: (x, y, w, h)
-        img.draw_rectangle(goal_blob.rect())
+        angle, distance, confidence, goal_blob = current_goal
+        img.draw_rectangle(goal_blob.rect)
+
+        # Visual Overlay: Draw tracking line from bottom-center to goal center
+        img.draw_line(
+            (BOTTOM_CENTER_X, BOTTOM_CENTER_Y, goal_blob.cx, goal_blob.cy), thickness=2
+        )
+
+        # Visual Overlay: Draw target center point
+        img.draw_cross(
+            (goal_blob.cx, goal_blob.cy), size=5
+        )
+
     else:
         angle = 254
+        distance = 254
+        confidence = 254
         goal_blob = None
 
     received = 0
@@ -124,6 +124,7 @@ while True:
 
     if received == 255:
         uart.write(bytes([angle + 70]))
+        uart.write(bytes[distance])
+        uart.write(bytes[confidence])
 
-    print(angle, clock.fps())
-    print(csi0.gain_db(), csi0.exposure_us(), csi0.rgb_gain_db())
+    print("Angle:", angle, "Dist:", distance, "FPS:", clock.fps())
